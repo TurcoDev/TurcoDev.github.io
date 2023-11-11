a06ec98f20dca557c956af1f4292042807cb1414
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-11-10 22:48:32 -0300

    Add Cover component with particles

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import '../styles/App.scss'
-import UnderConstruction from './components/UnderConstruction'
-// import Type from './components/Type'
+import { Cover } from './components/Cover/Cover'
+// import UnderConstruction from './components/UnderConstruction'
+import Type from './components/Type'
 
 
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
     <>
-      <UnderConstruction />
+      {/* <UnderConstruction /> */ }
+      <div style={{ padding: 50, textAlign: "center",  }}>
+        <Type />
+      </div>
+      < Cover />
+
     </>
   )
 }
---
+++ src/components/Cover/Cover.tsx
@@ -0,0 +1,24 @@
+import { useCallback } from "react"
+import Particles from "react-particles"
+import { loadFull } from "tsparticles"
+import type { Engine } from 'tsparticles-engine'
+import { optionsParticles } from "./particleOptions"
+
+
+export function Cover() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine)
+  }, [])
+
+  const particlesLoaded = useCallback(async () => { }, [])
+
+  return (
+    <div id="cover">
+      <Particles
+        className="absolute w-full h-full translate-z-0"
+        id="tsparticles" init={particlesInit} loaded={particlesLoaded}
+        options={optionsParticles} />
+      {/* <Introduction /> */}
+    </div>
+  )
+}
